a31d3aadad88e61c5a3221bbd6d32d8c038b90e2
Mario Vasilev <mariovasilev212@gmail.com>
2021-08-08 11:04:14 +0100

    changes to scraper
    user can specify whether to truncate or append to file

--- BERT/top100GutenbergScraper.py
+++ BERT/top100GutenbergScraper.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 
 
 def _get_web_pages_of_books(num_books=100):
     """
     Gets the links to the pages of the 100 top books yesterday on project Gutenberg;
-    :return: URLs to the "Top 100 EBooks yesterday" from project Gutenberg;
+    :return: list of URLs to homepages for each of the "Top 100 EBooks yesterday" from project Gutenberg;
     """
     various_charts = "https://www.gutenberg.org/browse/scores/top"
     base_url = "https://www.gutenberg.org"
@@ -19,7 +20,7 @@ def _get_links_to_text(books_menu_pages):
     """
     Stores the "Read this book online:HTML" pages of each book in links_to_text;
     :param books_menu_pages: list of urls to menu pages of books;
-    :return: list of urls to the "Read this book online:HTML" page of each book;
+    :return: list of urls to the "Read this book online:HTML" page for each book;
     """
     base_url = "https://www.gutenberg.org"
     links_to_text = []
@@ -30,46 +31,56 @@ def _get_links_to_text(books_menu_pages):
     return links_to_text
 
 
-def _scrape_books(links_to_text, file_name):
+def _scrape_books(links_to_text, file_name, truncate=True):
     """
     Scrape books from the links in links_to_text and save them in
     ../data/<file_name>
     :param links_to_text: urls to HTML of books;
     :param file_name: where to store the books;
-    :return: -1 if ../data/file_name exists, 1 otherwise;
+    :param truncate: whether to truncate if the path already exists, defaults to True in which
+    case it truncates; If False, it appends;
+    :return: void;
     """
+    carry_on = 'y'
     if os.path.exists(f"../data/{file_name}"):
-        return -1
+        carry_on = input("A file with that name already exists, if truncate is True I will overwrite it. Continue [y/n]:")
+    if carry_on == 'n':
+        return
     if not os.path.exists("../data"):
         os.mkdir("../data")
     save_path = os.path.join("../data/", file_name)
 
+    # select mode based on truncate;
+    mode = 'w' if truncate else 'a'
+
+    # print some info;
     print(f"downloading {len(links_to_text)} books...")
-    i = 0
+
     for url in links_to_text:
         # open request
         with requests.get(url) as f:
             soup = BeautifulSoup(f.content, "html.parser")
 
         # save to file;
-        with open(save_path, 'a') as books:
-            books.write(f"<book_{i}>\n")
+        # before the content of a book it has the following indicator:
+        # <book_firstName_secondName_authorName>
+        # a regex is there to get rid of ',.' characters and replace with '\s';
+        with open(save_path, mode) as books:
+            books.write(f"<book_{'_'.join(re.sub('[,.]+', ' ', soup.title.get_text()).strip().lower().split())}>\n")
             for p in soup.select("body p"):
                 books.write(p.get_text())
             books.write('\n')
-        i += 1
-    return 0
+        # stop truncation after first book;
+        if mode == 'w':
+            mode = 'a'
 
 
-def save_gutenberg_books(file_name, num_books=100):
+def save_gutenberg_books(file_name, num_books=100, truncate=True):
     """
     Saves books in ../data/file_name;
     :param file_name: name of file to store the books;
     :param num_books: choose how many books to scrape [1, 100];
-    :return: -1 if path already exists, 1 otherwise;
+    :param truncate: boolean, if True, truncates the contents of file_name, else appends to file_name;
+    :return: void;
     """
-    return _scrape_books(_get_links_to_text(_get_web_pages_of_books(num_books)), file_name)
-
-
-if __name__ == "__main__":
-    save_gutenberg_books("gutenberg_books.txt", 5)
+    _scrape_books(_get_links_to_text(_get_web_pages_of_books(num_books)), file_name, truncate)
