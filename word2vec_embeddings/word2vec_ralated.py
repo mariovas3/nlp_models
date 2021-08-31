138df89823c35850ce2f115b0c8c97aaf0fa8ae8
Mario Vasilev <mariovasilev212@gmail.com>
2021-08-31 07:01:52 +0100

    adding subsampling and util for getting (centers, contexts, negatives) triplets

--- word2vec_embeddings/word2vec_ralated.py
+++ word2vec_embeddings/word2vec_ralated.py
@@ -5,6 +5,8 @@ import sys
 sys.path.append("../preprocessing")
 import vocab as voc
 import random
+import math
+import itertools
 
 
 class NegativeSampleGenerator:
@@ -42,3 +44,49 @@ class NegativeSampleGenerator:
             num_negatives %= self.big_draw
         self.taken += num_negatives
         return result + self.candidates[self.taken-num_negatives:self.taken]
+
+
+def subsample(tokens, vocab, t=1e-5):
+    """
+    This is the subsampling mechanism proposed by Mikolov et. al. for discarding very frequent words
+    that carry little meaning (e.g. "the", "an", "a" etc...);
+    :param tokens: a list of tokenised sentences (i.e. a list of lists);
+    :param vocab: a vocab.Vocab object;
+    :param t: this is a heuristic for discarding very frequent words; Depends on sample size and the construction
+    of your corpus (since higher t leads to lower probability to discard any given word);
+    :return: a list of lists of token_ids based on the mapping from vocab;
+    """
+    counts = voc.count_corpus(tokens)
+    available_tokens = sum(counts.values())
+    def discard(token, counts, available_tokens):
+        """
+        Simple utility function returns True if U(0, 1) <= max( 1 - sqrt(t/f(w)), 0)
+        where f(w) = num_occurs(w) / corpus_size; and False otherwise;
+        :param token: string object;
+        :param counts: dict object with (string, int) pairs;
+        :param available_tokens: len of corpus;
+        :return: boolean based on the logic defined in the docstring;
+        """
+        return random.random() <= max(1 - math.sqrt(t / counts[token] * available_tokens), 0)
+    samples = [[vocab[token] for token in line if discard(token, counts, available_tokens)] for line in tokens]
+
+
+def _get_triplets(sentence, negatives_sampler, num_negatives, window_size):
+    """
+    Gets centers, contexts, negatives per a sentence which is of the form List[int] (i.e. each element of sentence
+    is a token_id);
+    :param sentence: len(sentence) > 2;
+    :param negatives_sampler: object of type NegativeSampleGenerator;
+    :param num_negatives: int type, how many negatives to sample;
+    :param window_size: int type, the size of context window;
+    :return: zipped triplet centers, contexts, negatives;
+    """
+    contexts = []
+    negatives = []
+    centers = sentence
+    for i in range(len(sentence)):
+        curr_contexts = [sentence[j] for j in range(max(0, i - window_size), min(len(sentence) - 1, i + window_size))
+                         if j != i]
+        contexts.append(curr_contexts)
+        negatives.append(negatives_sampler.sample(num_negatives))
+    return zip(centers, contexts, negatives)
